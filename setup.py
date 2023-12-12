03e6b839f2ba0b4dc3e4b4960a4d64317d1db414
aDAVISk <akito.davis@gmail.com>
2023-12-12 23:44:48 +0900

    Added gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 
 # ref: https://qiita.com/studio_haneya/items/9aad8f9ede11e58b41a8
+# ref: https://qiita.com/shonansurvivors/items/0fbcbfde129f2d26301c
 
 from setuptools import setup, find_packages
+
+# long_description(後述)に、GitHub用のREADME.mdを指定
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='xyztilefile',  # パッケージ名（pip listで表示される）
@@ -10,5 +15,12 @@ setup(
     author='aDAVISk',  # 作者名
     license='MIT',  # ライセンス
     package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src')
+    packages=setuptools.find_packages(where='src'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='xyztilefile xyz-tile xyztile',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
