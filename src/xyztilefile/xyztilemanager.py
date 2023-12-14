fa4c133432823c08905e445fd71fdcf127f64b2a
aDAVISk <akito.davis@gmail.com>
2023-12-14 14:57:38 +0900

    Update xyztilemanager.py

--- src/xyztilefile/xyztilemanager.py
+++ src/xyztilefile/xyztilemanager.py
@@ -1,6 +1,8 @@
 import os
 from .xyztilefile import *
 
+_convfunc = lambda x : x
+
 class XYZTileManager:
     """Utilize locally saved cache files with the source
 
@@ -10,16 +12,17 @@ class XYZTileManager:
     The retrieve data is automatically saved to local cache file.
 
     """
-    def __init__(self, srcbase:str, cachebase:str):
-        self.__cache = {}
-        self.__src = XYZTileFile(srcbase, cache=self.__cache)
-        self.__lcl = XYZHttpTileFile(cachebase, cache=self.__cache)
+    def __init__(self, srcbase:str, cachebase:str, convfunc=_convfunc):
+        self._cache = {}
+        self._src = XYZTileFile(srcbase, cache=self._cache)
+        self._lcl = XYZHttpTileFile(cachebase, cache=self._cache)
+        self._convfunc = convfunc
 
     def get(self, x:int, y:int, z:int):
         try:
-            return self.__lcl.get(x,y,z)
+            return self._lcl.get(x,y,z)
         except OSError:
             pass
-        res = self.__src.get(x,y,z)
+        res = self._src.get(x,y,z)
         self.__save(x,y,z)
         return res
