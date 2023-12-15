c20125e2956cc0fb9b987acf7cac33a226a15001
aDAVISk <akito.davis@gmail.com>
2023-12-15 14:09:16 +0900

    Tested npy

---
+++ test/test_npy.py
@@ -0,0 +1,30 @@
+import pprint
+import numpy as np
+from xyztilefile import *
+
+pp = pprint.PrettyPrinter(indent=3)
+
+x, y, z = calc_xyz_from_lonlat(135.0,35.0,15)
+xyz = calc_xyz_from_lonlat
+
+ar = np.arange(12).reshape((4,3))
+print("Initial")
+test = XYZTileFile("./tile_sample/{z}/{x}/{y}.npy")
+pp.pprint(test)
+
+print()
+print("Set array")
+test.set(x,y,z,ar)
+pp.pprint(test)
+
+print()
+print("Save & clear cache")
+test.save(x,y,z)
+test.clc()
+pp.pprint(test)
+
+print()
+print("Load the saved array")
+ar2 = test.get(*xyz(135.0,35.0,15))
+pp.pprint(ar2)
+pp.pprint(test)
